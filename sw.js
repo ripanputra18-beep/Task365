91ebc6b9aea2092d21165e6aa1f1129b1df20659
ripanputra18-beep <ripanputra18@gmail.com>
2026-08-03 12:43:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ke-hoach-shell-v1.7-drive";
+const CACHE = "ke-hoach-shell-v1.8-drive";
 const SHELL = ["./", "./styles.css", "./app.js", "./config.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
